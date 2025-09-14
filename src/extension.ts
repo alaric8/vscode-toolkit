44cc91d3a189381de26a28561c64dc127bb5de59
alaric851 <alaric851@gmail.com>
2025-09-14 11:45:35 +0800

    fix: 删除多余代码

--- src/extension.ts
+++ src/extension.ts
@@ -1,16 +1,8 @@
 import {
   defineExtension,
-  useCommand,
-  useCommentController,
-  useIsDarkTheme,
-  watchEffect,
 } from "reactive-vscode";
 import { logger } from "./utils";
 
 export = defineExtension(() => {
   logger.info("Extension Activated");
-  const isDark = useIsDarkTheme();
-  watchEffect(() => {
-    logger.info("Is Dark Theme:", isDark.value);
-  });
 });
