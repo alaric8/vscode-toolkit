4f5e5427434eedaeaa8dbc90d397db3dc30e40df
alaric851 <alaric851@gmail.com>
2025-09-14 11:31:26 +0800

    333

--- src/extension.ts
+++ src/extension.ts
@@ -5,13 +5,10 @@ import {
   useIsDarkTheme,
   watchEffect,
 } from "reactive-vscode";
-import { window } from "vscode";
-import { message } from "./configs";
 import { logger } from "./utils";
 
 export = defineExtension(() => {
   logger.info("Extension Activated");
-
   const isDark = useIsDarkTheme();
   watchEffect(() => {
     logger.info("Is Dark Theme:", isDark.value);
